2720cc0deb09c8ced9e98679582587dcde494c1f
jcelsocosta <jccl@cin.ufpe.br>
2020-08-30 11:05:59 -0300

    Adicionando angular-material

--- teorienta/src/app/app.module.ts
+++ teorienta/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginAdminComponent } from './login-admin/login-admin.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { LoginAdminComponent } from './login-admin/login-admin.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
