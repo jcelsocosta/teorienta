e9299afbc998f24e72c161e0f75d5b15057449ec
jcelsocosta <jccl@cin.ufpe.br>
2020-11-14 21:32:38 -0300

    adicionando arquivos de configuracao para tests de aceitacao

---
+++ tests-acceptance/stepdefinitions/announcement.ts
@@ -0,0 +1,11 @@
+import { defineSupportCode } from 'cucumber';
+import { browser, $, element, ElementArrayFinder, by } from 'protractor';
+let chai = require('chai').use(require('chai-as-promised'));
+let expect = chai.expect;
+declare var  require : any;
+
+
+
+defineSupportCode(function ({ Given, When,Then}) {
+   
+})
