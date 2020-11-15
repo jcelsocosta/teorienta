b9282f628f0dec0ad385ffea7401365444a007d7
jcelsocosta <jccl@cin.ufpe.br>
2020-11-14 21:40:55 -0300

    adicionando o outDir para a pasta compiledCode

---
+++ tests-acceptance/compiledCode/stepdefinitions/announcement.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var cucumber_1 = require("cucumber");
+var chai = require('chai').use(require('chai-as-promised'));
+var expect = chai.expect;
+cucumber_1.defineSupportCode(function (_a) {
+    var Given = _a.Given, When = _a.When, Then = _a.Then;
+});
